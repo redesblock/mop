1027a9de3bf5e84d297bb7077ecd99a4c0ce5f5b
gcrocky <gcrocky@users.noreply.github.com>
2022-06-22 09:32:59 +0800

    bump version 0.4.0

--- cmd/hop/version.go
+++ cmd/hop/version.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	version = "0.3.0" // manually set semantic version number
+	version = "0.4.0" // manually set semantic version number
 	commit  string    // automatically set git commit hash
 
 	Version = func() string {
