394d9a4ea209a7ff8c1e12efd85c402f3b4e1f8f
flyStar2008 <flystar@redeslab.io>
2022-11-27 21:57:44 +0800

    goroutine id

--- core/util/gid.go
+++ core/util/gid.go
@@ -9,7 +9,7 @@ import (
 func GetGID() uint64 {
 	b := make([]byte, 64)
 	b = b[:runtime.Stack(b, false)]
-	b = bytes.TrimPrefix(b, []byte("goroutine"))
+	b = bytes.TrimPrefix(b, []byte("goroutine "))
 	b = b[:bytes.IndexByte(b, ' ')]
 	n, _ := strconv.ParseUint(string(b), 10, 64)
 	return n
