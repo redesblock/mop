fa94c5ef62a6d896037be3db94ef0ae07a2798ee
sidtt <sidtt@users.noreply.github.com>
2023-01-19 23:15:36 +0800

    revert mode_get

--- core/storer/localstore/mode_get_multi.go
+++ core/storer/localstore/mode_get_multi.go
@@ -3,7 +3,6 @@ package localstore
 import (
 	"context"
 	"errors"
-	"github.com/redesblock/mop/core/storer/sharky"
 	"time"
 
 	"github.com/redesblock/mop/core/cluster"
@@ -48,36 +47,43 @@ func (db *DB) GetMulti(ctx context.Context, mode storage.ModeGet, addrs ...clust
 func (db *DB) getMulti(ctx context.Context, mode storage.ModeGet, addrs ...cluster.Address) (out []shed.Item, err error) {
 	out = make([]shed.Item, len(addrs))
 	for i, addr := range addrs {
-		out[i].Address = addr.Bytes()
-	}
-
-	err = db.retrievalDataIndex.Fill(out)
-	if err != nil {
-		return nil, err
-	}
-
-	for i, item := range out {
-		l, err := sharky.LocationFromBinary(item.Location)
-		if err != nil {
+		if item, err := db.get(ctx, mode, addr); err != nil {
 			return nil, err
-		}
-
-		out[i].Data = make([]byte, l.Length)
-		err = db.sharky.Read(ctx, l, out[i].Data)
-		if err != nil {
-			return nil, err
+		} else {
+			out[i] = item
 		}
 	}
+	// for i, addr := range addrs {
+	// 	out[i].Address = addr.Bytes()
+	// }
 
-	switch mode {
-	// update the access timestamp and gc index
-	case storage.ModeGetRequest:
-		//db.updateGCItems(out...)
+	// err = db.retrievalDataIndex.Fill(out)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	// no updates to indexes
-	case storage.ModeGetSync, storage.ModeGetLookup:
-	default:
-		return out, ErrInvalidMode
-	}
+	// for i, item := range out {
+	// 	l, err := sharky.LocationFromBinary(item.Location)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+
+	// 	out[i].Data = make([]byte, l.Length)
+	// 	err = db.sharky.Read(ctx, l, out[i].Data)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// }
+
+	// switch mode {
+	// // update the access timestamp and gc index
+	// case storage.ModeGetRequest:
+	// 	db.updateGCItems(out...)
+
+	// // no updates to indexes
+	// case storage.ModeGetSync, storage.ModeGetLookup:
+	// default:
+	// 	return out, ErrInvalidMode
+	// }
 	return out, nil
 }
